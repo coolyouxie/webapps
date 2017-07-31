c3405ef229b91faa4f20541badb613d4f5a934e6
coolyouxie <xie-shuai@163.com>
2017-07-31 23:33:27 +0800

    添加行政区查询功能

--- src/main/java/com/webapps/controller/ProvinceController.java
+++ src/main/java/com/webapps/controller/ProvinceController.java
@@ -1,9 +1,17 @@
 package com.webapps.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.webapps.common.entity.Province;
 import com.webapps.service.IProvinceService;
 
 @Controller
@@ -21,6 +29,51 @@ public class ProvinceController {
 			e.printStackTrace();
 		}
 		return "";
+	}
+	
+	/**
+	 * 根据父级行政ID找到父级行政区下所有的子行政区
+	 * @param model
+	 * @param parentId
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("/queryProvinceByParentId")
+	public String queryProvinceByParentId(Model model,Integer parentId,HttpServletRequest request,HttpServletResponse response){
+		if(parentId!=null){
+			try {
+				List<Province> provinces = iProvinceService.queryProvinceByParentId(parentId);
+				model.addAttribute("provinces", provinces);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * 根据行政区等级查询所有行政区
+	 * @param model
+	 * @param level
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("/queryProvinceByLevel")
+	public String queryProvinceByLevel(Model model,Integer level,HttpServletRequest request,HttpServletResponse response){
+		if(level!=null){
+			try {
+				List<Province> provinces = iProvinceService.queryProvinceByLevel(level);
+				model.addAttribute("provinces", provinces);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return null;
 	}
 
 }
