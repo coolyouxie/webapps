cd19effad8106863561b2a1b06cd8d0bd159c36c
coolyouxie <xie-shuai@163.com>
2017-08-29 22:10:46 +0800

    刚才提交的方法名改错，尴尬

--- src/main/java/com/webapps/controller/AppController.java
+++ src/main/java/com/webapps/controller/AppController.java
@@ -65,8 +65,8 @@ public class AppController {
 	}
 	
 	@ResponseBody
-	@RequestMapping(value="/getValidateCode", method=RequestMethod.POST, produces="text/html;charset=UTF-8")
-	public String getValidateCode(@RequestBody String params){
+	@RequestMapping(value="/register", method=RequestMethod.POST, produces="text/html;charset=UTF-8")
+	public String register(@RequestBody String params){
 		Gson gson = new Gson();
 		AliSmsMsg asm = gson.fromJson(params, AliSmsMsg.class);
 		ResultDto<AliSmsMsg> dto = iAliSmsMsgService.getAliSmsCode(asm.getPhoneNumbers(), asm.getType());
@@ -81,7 +81,10 @@ public class AppController {
 	@ResponseBody
 	@RequestMapping(value="/getValidatCode")
 	public String getValidatCode(String params){
-		return null;
+		Gson gson = new Gson();
+		AliSmsMsg asm = gson.fromJson(params, AliSmsMsg.class);
+		ResultDto<AliSmsMsg> dto = iAliSmsMsgService.getAliSmsCode(asm.getPhoneNumbers(), asm.getType());
+		return JSONUtil.toJSONString(JSONUtil.toJSONObject(dto));
 	}
 	
 	@ResponseBody
