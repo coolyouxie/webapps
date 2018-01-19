f674271db08f11d479d0e9e559a3294f0ffbddde
Sundy520 <xie-shuai@163.com>
2018-01-19 17:54:37 +0800

    添加表修改sql及统计数据部分功能。

---
+++ src/main/java/com/webapps/mapper/IRateDtoMapper.java
@@ -0,0 +1,8 @@
+package com.webapps.mapper;
+
+import com.webapps.common.entity.ApplyExpenditure;
+import com.webapps.common.form.ApplyExpenditureRequestForm;
+
+public interface IRateDtoMapper extends IBaseMapper<ApplyExpenditure>,IPageMapper<ApplyExpenditure,ApplyExpenditureRequestForm> {
+
+}
